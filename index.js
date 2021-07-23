92c10197725fcfd5246fb8511ce7d2130ab0c612
RussellDash332 <russellsaerang@gmail.com>
2021-07-23 17:40:00 +0800

    Update weather link

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let DATA = {
 
 async function setWeatherInformation() {
   await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Singapore,sg&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
   )
     .then(r => r.json())
     .then(r => {
